d77870cf305eec62ad3172d863ab48546525d95d
Sabbir386 <sabbir386.sa@gmail.com>
2023-06-08 00:13:14 +0600

    database connected to mongodb

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 
 
-console.log(process.env.DB_USER);
+
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.z2phr5m.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -28,7 +28,7 @@ async function run() {
     try {
         // Connect the client to the server	(optional starting in v4.7)
         client.connect();
-        const toyCollection = client.db('toyData').collection('toy');
+        const classesCollection = client.db('classesData').collection('class');
 
         // Send a ping to confirm a successful connection
         await client.db("admin").command({ ping: 1 });
@@ -37,70 +37,71 @@ async function run() {
 
 
 
-        app.post('/addToy', async (req, res) => {
-            const body = req.body;
-            const result = await toyCollection.insertOne(body);
-            res.send(result);
+        // app.post('/addToy', async (req, res) => {
+        //     const body = req.body;
+        //     const result = await toyCollection.insertOne(body);
+        //     res.send(result);
 
-        })
-        app.get('/allToys', async (req, res) => {
-            const result = await toyCollection.find({}).toArray();
-            res.send(result);
-
-        })
-
-        app.put('/allToys/:updateId', async (req, res) => {
-            const id = req.params.updateId;
-            const filter = { _id: new ObjectId(id) }
-            const options = { upsert: true };
-            const updateToy = req.body;
-            const toy = {
-                $set: {
-
-                    name: updateToy.name,
-                    photoUrl: updateToy.photoUrl,
-                    sellerName: updateToy.sellerName,
-                    sellerEmail: updateToy.sellerEmail,
-                    subCategory: updateToy.subCategory,
-                    price: updateToy.price,
-                    ratings: updateToy.ratings,
-                    availableQuantity: updateToy.availableQuantity,
-                    availableQuantity: updateToy.availableQuantity,
-                }
-            }
-            const result = await toyCollection.updateOne(filter, toy, options);
+        // })
+        app.get('/allClasses', async (req, res) => {
+            const result = await classesCollection.find({}).toArray();
+            console.log(result);
             res.send(result);
 
         })
 
+        // app.put('/allToys/:updateId', async (req, res) => {
+        //     const id = req.params.updateId;
+        //     const filter = { _id: new ObjectId(id) }
+        //     const options = { upsert: true };
+        //     const updateToy = req.body;
+        //     const toy = {
+        //         $set: {
+
+        //             name: updateToy.name,
+        //             photoUrl: updateToy.photoUrl,
+        //             sellerName: updateToy.sellerName,
+        //             sellerEmail: updateToy.sellerEmail,
+        //             subCategory: updateToy.subCategory,
+        //             price: updateToy.price,
+        //             ratings: updateToy.ratings,
+        //             availableQuantity: updateToy.availableQuantity,
+        //             availableQuantity: updateToy.availableQuantity,
+        //         }
+        //     }
+        //     const result = await toyCollection.updateOne(filter, toy, options);
+        //     res.send(result);
+
+        // })
 
 
-        app.delete('/allToys/:id', async (req, res) => {
-            const id = req.params.id;
-            console.log(id);
-            const query = { _id: new ObjectId(id) };
-            const result = await toyCollection.deleteOne(query);
-            res.send(result);
-        })
-        app.get('/allToys/:Category', async (req, res) => {
-            if (req.params.Category == 'MathToys') {
-                const result = await toyCollection.find({ subCategory: req.params.Category }).toArray();
-                return res.send(result);
-            }
-            else if (req.params.Category == 'EngineeringToys') {
-                const result = await toyCollection.find({ subCategory: req.params.Category }).toArray();
-                return res.send(result);
-            }
-            else if (req.params.Category == 'LanguageToys') {
-                const result = await toyCollection.find({ subCategory: req.params.Category }).toArray();
-                return res.send(result);
-            }
-            else {
-                const result = await toyCollection.find({}).toArray();
-                return res.send(result);
-            }
 
-        })
+        // app.delete('/allToys/:id', async (req, res) => {
+        //     const id = req.params.id;
+        //     console.log(id);
+        //     const query = { _id: new ObjectId(id) };
+        //     const result = await toyCollection.deleteOne(query);
+        //     res.send(result);
+        // })
+        // app.get('/allToys/:Category', async (req, res) => {
+        //     if (req.params.Category == 'MathToys') {
+        //         const result = await toyCollection.find({ subCategory: req.params.Category }).toArray();
+        //         return res.send(result);
+        //     }
+        //     else if (req.params.Category == 'EngineeringToys') {
+        //         const result = await toyCollection.find({ subCategory: req.params.Category }).toArray();
+        //         return res.send(result);
+        //     }
+        //     else if (req.params.Category == 'LanguageToys') {
+        //         const result = await toyCollection.find({ subCategory: req.params.Category }).toArray();
+        //         return res.send(result);
+        //     }
+        //     else {
+        //         const result = await toyCollection.find({}).toArray();
+        //         return res.send(result);
+        //     }
+
+        // })
 
 
 
@@ -112,11 +113,38 @@ async function run() {
 }
 run().catch(console.dir);
 
+// const { MongoClient, ServerApiVersion } = require('mongodb');
+// const uri = "mongodb+srv://<username>:<password>@cluster0.z2phr5m.mongodb.net/?retryWrites=true&w=majority";
+
+// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
+// const client = new MongoClient(uri, {
+//   serverApi: {
+//     version: ServerApiVersion.v1,
+//     strict: true,
+//     deprecationErrors: true,
+//   }
+// });
+
+// async function run() {
+//   try {
+//     // Connect the client to the server	(optional starting in v4.7)
+//     await client.connect();
+//     // Send a ping to confirm a successful connection
+//     await client.db("admin").command({ ping: 1 });
+//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
+//   } finally {
+//     // Ensures that the client will close when you finish/error
+//     await client.close();
+//   }
+// }
+// run().catch(console.dir);
+
+
 
 
 app.get('/', (req, res) => {
-    res.send('Education toys running');
+    res.send('Brilliance Institude running');
 })
 app.listen(port, () => {
-    console.log(`Educational toys running on port ${port}`);
+    console.log(`Brilliance Institude running on port ${port}`);
 })
