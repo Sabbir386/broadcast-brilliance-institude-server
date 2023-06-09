e84ece3b41f92608ce12796b61fecdd211c85374
Sabbir386 <sabbir386.sa@gmail.com>
2023-06-09 21:13:06 +0600

    added jwt implementation unauthorized access users

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 
 const express = require('express');
 const jwt = require('jsonwebtoken');
+const morgan = require('morgan');
+
 
 const cors = require('cors');
 const app = express();
@@ -11,9 +13,12 @@ const port = process.env.PORT || 5000;
 // middleware
 app.use(express.json());
 app.use(cors());
+app.use(morgan('dev'));
+
 
 const verifyJWT = (req, res, next) => {
     const authorization = req.headers.authorization;
+    console.log(authorization);
     if (!authorization) {
         return res.status(401).send({ error: true, message: 'Unauthorized access' });
     }
@@ -71,7 +76,7 @@ async function run() {
         // jWT TOKEN 
         app.post('/jwt', (req, res) => {
             const user = req.body;
-            const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '9h' })
+            const token = jwt.sign(user, process.env.TOKEN_SECRET, { expiresIn: '5h' })
 
             res.send({ token })
         })
@@ -126,7 +131,7 @@ async function run() {
         //selected booking class
         app.post('/bookingClass', async (req, res) => {
             const body = req.body;
-            const result = await usersCollection.insertOne(body);
+            const result = await usersSelectedCollection.insertOne(body);
             res.send(result);
 
         })
@@ -137,24 +142,23 @@ async function run() {
                 res.send([]);
             }
             const decodedEmail = req.decoded.email;
+            // console.log('user email :', email);
+            // console.log('decodedEmail email :', decodedEmail);
             if (email !== decodedEmail) {
                 return res.status(403).send({ error: true, message: 'Forbiddden access' })
             }
             const query = { email: email };
             const result = await usersSelectedCollection.find(query).toArray();
+
             res.send(result);
         })
 
         app.delete('/bookingClass/:id', async (req, res) => {
             const id = req.params.id;
-            console.log(id);
-
             const query = { _id: new ObjectId(id) };
             const result = await usersSelectedCollection.deleteOne(query);
             res.send(result);
         })
-
-
 
         //instructors api
         app.get('/allinstructors', async (req, res) => {
@@ -163,95 +167,12 @@ async function run() {
 
         })
 
-        // app.put('/allToys/:updateId', async (req, res) => {
-        //     const id = req.params.updateId;
-        //     const filter = { _id: new ObjectId(id) }
-        //     const options = { upsert: true };
-        //     const updateToy = req.body;
-        //     const toy = {
-        //         $set: {
-
-        //             name: updateToy.name,
-        //             photoUrl: updateToy.photoUrl,
-        //             sellerName: updateToy.sellerName,
-        //             sellerEmail: updateToy.sellerEmail,
-        //             subCategory: updateToy.subCategory,
-        //             price: updateToy.price,
-        //             ratings: updateToy.ratings,
-        //             availableQuantity: updateToy.availableQuantity,
-        //             availableQuantity: updateToy.availableQuantity,
-        //         }
-        //     }
-        //     const result = await toyCollection.updateOne(filter, toy, options);
-        //     res.send(result);
-
-        // })
-
-
-
-        // app.delete('/allToys/:id', async (req, res) => {
-        //     const id = req.params.id;
-        //     console.log(id);
-        //     const query = { _id: new ObjectId(id) };
-        //     const result = await toyCollection.deleteOne(query);
-        //     res.send(result);
-        // })
-        // app.get('/allToys/:Category', async (req, res) => {
-        //     if (req.params.Category == 'MathToys') {
-        //         const result = await toyCollection.find({ subCategory: req.params.Category }).toArray();
-        //         return res.send(result);
-        //     }
-        //     else if (req.params.Category == 'EngineeringToys') {
-        //         const result = await toyCollection.find({ subCategory: req.params.Category }).toArray();
-        //         return res.send(result);
-        //     }
-        //     else if (req.params.Category == 'LanguageToys') {
-        //         const result = await toyCollection.find({ subCategory: req.params.Category }).toArray();
-        //         return res.send(result);
-        //     }
-        //     else {
-        //         const result = await toyCollection.find({}).toArray();
-        //         return res.send(result);
-        //     }
-
-        // })
-
-
-
-
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
     }
 }
 run().catch(console.dir);
-
-// const { MongoClient, ServerApiVersion } = require('mongodb');
-// const uri = "mongodb+srv://<username>:<password>@cluster0.z2phr5m.mongodb.net/?retryWrites=true&w=majority";
-
-// // Create a MongoClient with a MongoClientOptions object to set the Stable API version
-// const client = new MongoClient(uri, {
-//   serverApi: {
-//     version: ServerApiVersion.v1,
-//     strict: true,
-//     deprecationErrors: true,
-//   }
-// });
-
-// async function run() {
-//   try {
-//     // Connect the client to the server	(optional starting in v4.7)
-//     await client.connect();
-//     // Send a ping to confirm a successful connection
-//     await client.db("admin").command({ ping: 1 });
-//     console.log("Pinged your deployment. You successfully connected to MongoDB!");
-//   } finally {
-//     // Ensures that the client will close when you finish/error
-//     await client.close();
-//   }
-// }
-// run().catch(console.dir);
-
 
 
 
